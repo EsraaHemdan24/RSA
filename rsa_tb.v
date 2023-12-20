c4733b3e35de8bd39b45c7e83ac978838704aef0
Esraa Hemdan <81455019+EsraaHemdan24@users.noreply.github.com>
2023-12-20 22:58:17 +0200

    Update rsa_tb.v

--- rsa_tb.v
+++ rsa_tb.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   14:20:27 07/28/2023
-// Design Name:   rsa_top
-// Module Name:   /home/ise/ISE_files/Projects/rsa_draft/rsa_tb.v
-// Project Name:  rsa_draft
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: rsa_top
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+
 
 module rsa_tb;
 
