f9152850eae8250c8f3eacf52734fd7e1cc9aaae
Esraa Hemdan <81455019+EsraaHemdan24@users.noreply.github.com>
2023-12-20 22:57:55 +0200

    Update rsa_fpga.v

--- rsa_fpga.v
+++ rsa_fpga.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:15:09 07/28/2023 
-// Design Name: 
-// Module Name:    rsa_fpga 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module rsa_fpga(
 	 input CLK_IN1_P,
 	 input CLK_IN1_N,
