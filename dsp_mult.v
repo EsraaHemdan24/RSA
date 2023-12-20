be961017acd6f3f998db6b9392aef4b9b7154d75
Esraa Hemdan <81455019+EsraaHemdan24@users.noreply.github.com>
2023-12-20 22:57:38 +0200

    Update dsp_mult.v

--- dsp_mult.v
+++ dsp_mult.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:10:03 07/28/2023 
-// Design Name: 
-// Module Name:    dsp_mult 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-(*use_dsp48 = "yes"*)
+
 module dsp_mult(
     input [5:0] in1,
     input [5:0] in2,
